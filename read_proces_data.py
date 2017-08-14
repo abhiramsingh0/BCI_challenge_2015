1db14137ab15a362f63d7d644c86d30d118bf6c5
gnl <abhiram25.1990@gmail.com>
2017-08-15 00:15:17 +0530

    feature scaling

--- read_proces_data.py
+++ read_proces_data.py
@@ -121,5 +121,5 @@ def feature_scaling(features, batch_size, seq_len):
     X_scaled = np.zeros(features.shape)
     for index in range(0,batch_size):
         X_scaled[index,:seq_len[index],:] = \
-        preprocessing.scale(X_scaled[index,:seq_len[index],:])
+        preprocessing.scale(features[index,:seq_len[index],:])
     return X_scaled
